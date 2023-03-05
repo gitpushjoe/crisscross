764b559cccf9c54282b01938e9a1c476a70bc0e0
gitpushjoe <josephobaraye@gmail.com>
2023-03-04 22:39:35 -0500

    fleshed out shell UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Shell from './Shell'
 
 const root : HTMLElement = document.getElementById('root')!;
 if (root !== null) {
     ReactDOM.createRoot(root).render(
         <React.StrictMode>
-            <App />
+            <Shell />
         </React.StrictMode>
     )
 }
